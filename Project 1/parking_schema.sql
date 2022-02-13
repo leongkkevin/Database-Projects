634f1147a6460e4aa6ab1f141d622b7c258080e0
leongkkevin <kkleong@smu.edu>
2022-02-12 18:00:56 -0600

    Added schema 1

--- Project 1/parking_schema.sql
+++ Project 1/parking_schema.sql
@@ -1,1 +1,71 @@
 CREATE DATABASE parking_db;
+CREATE TABLE car (
+    license_plate VARCHAR(10) PRIMARY KEY,
+    vehicle_type VARCHAR(50),
+
+    fan INT,
+    FOREIGN KEY (fan) REFERENCES fan(id)
+);
+
+CREATE TABLE fan(
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE event(
+    name VARCHAR(50),
+    event_date DATE,
+    PRIMARY KEY (name, event_date),
+
+    start_time TIME,
+    end_time TIME
+);
+
+CREATE TABLE parking_space (
+    number INT,
+    lot VARCHAR(2),
+    car VARCHAR(10),
+    available BOOLEAN,
+    handicap BOOLEAN,
+    FOREIGN KEY (lot) REFERENCES parking_lot(name),
+    PRIMARY KEY (number, lot)
+);
+
+CREATE TABLE parking_lot (
+    name VARCHAR(2) PRIMARY KEY,
+    stadium VARCHAR(30),
+    FOREIGN KEY (stadium) REFERENCES stadium(name)
+);
+
+CREATE TABLE stadium (
+    name VARCHAR(30) PRIMARY KEY,
+    address VARCHAR(50)
+);
+
+CREATE TABLE allocation(
+    id INT PRIMARY KEY,
+
+    parking_lot VARCHAR(2),
+    parking_space INT,
+
+    car VARCHAR(10),
+    employee INT,
+
+    event_name VARCHAR(50),
+    event_date DATE,
+
+    FOREIGN KEY (parking_lot, parking_space)
+        REFERENCES parking_space(number, lot),
+
+    FOREIGN KEY (event_name, event_date)
+        REFERENCES event(name, event_date)
+);
+
+CREATE TABLE parking_employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    stadium VARCHAR(30),
+    parking_lot VARCHAR(2),
+    FOREIGN KEY (stadium) REFERENCES stadium(name),
+    FOREIGN KEY (parking_lot) REFERENCES parking_lot(name)
+);
