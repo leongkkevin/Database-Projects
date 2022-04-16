72bbb5bde1dcf4236b5c2eff10c56a9f755c006c
leongkkevin <kkleong@smu.edu>
2022-04-15 22:26:59 -0500

    Finished parking_space query

--- Project 2/app/routes/parking_space.js
+++ Project 2/app/routes/parking_space.js
@@ -3,14 +3,9 @@ const router = express.Router();
 const parking_space = require('../models/parking_space');
 
 router.get('/', async (req, res, next) => {
-    try {
-        console.log(req.query)
-        const spots = await parking_space.fetchSpots(req.query.stadium, req.query.lot, req.query.available)
-        res.sendStatus(200).json(spots);
-    } catch (err) {
-        console.error('Failed to load current user:', err);
-        res.sendStatus(500).json({ message: err.toString() });
-    }
+    const spots = await parking_space.fetchSpots(req.query.stadium, req.query.lot, req.query.available);
+    res.json(spots);
+    next();
 });
 
 module.exports = router;
